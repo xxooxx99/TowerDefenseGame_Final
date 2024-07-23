f79e1236f6b1fcf934751f87c7a4022092bfc104
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-07-23 11:56:55 +0900

    ✨ update : 회원가입, 로그인 기능만 추가

--- server/init/socket.js
+++ server/init/socket.js
@@ -1,13 +1,40 @@
 import { Server as SocketIO } from 'socket.io';
-import connectHandler from '../handler/index.js';
+import { activeSessions } from '../app.js';
 
 const initSocket = (server) => {
-  const io = new SocketIO();
-  io.attach(server); // Socket.IO 서버와 통합
+  const io = new SocketIO(server);
 
-  connectHandler(io);
+  io.on('connection', (socket) => {
+    console.log(`New user connected: ${socket.id}`);
 
-  return io;
+    socket.on('event', (packet) => {
+      console.log(
+        `Received packet: ${JSON.stringify(`패킷 타입 : ${packet.packetType} 유저 아이디 : ${packet.userId}`)}`,
+      );
+
+      socket.userId = packet.userId;
+
+      switch (packet.packetType) {
+        /**
+        여기에 패킷타입 추가 
+        case PacketType.C2S_MATCH_REQUEST:
+        handleMatchRequest(socket, packet);
+        break;
+        */
+        default:
+          console.log(`Unknown packet type: ${packet.packetType}`);
+      }
+    });
+
+    socket.in('disconnect', () => {
+      console.log(`User disconnected: ${socket.id}`);
+
+      if (socket.userId && activeSessions[socket.userId]) {
+        delete activeSessions[socket.userId];
+        console.log(`Session for user ${socket.userId} has been invalidated`);
+      }
+    });
+  });
 };
 
 export default initSocket;
